637cf5fb6a05108b619213f442b8efbfe0c9caef
gease <gease@mail.ru>
2022-10-19 20:29:22 +0200

    Clear file input on upload.

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -1,13 +1,17 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 import {Storage} from "aws-amplify";
 
 function FileUpload({fetchCallback}) {
-    let [file, setFile] = useState(null);
-    let [level, setLevel] = useState('public');
+    const [file, setFile] = useState(null);
+    const [level, setLevel] = useState('public');
+    const uploadRef = useRef();
     const onFormSubmit = async (event) => {
         event.preventDefault();
         await Storage.put(file.name, file, {level});
+        setLevel('public');
+        setFile(null);
         fetchCallback();
+        uploadRef.current.value = null;
     }
 
     const onRadiosChange = (event) => {
@@ -19,7 +23,7 @@ function FileUpload({fetchCallback}) {
     return (
         <form onSubmit={onFormSubmit}>
             <label htmlFor='upload'>Upload a file</label>
-            <input type='file' name='upload' id="upload" onChange={(event) => setFile(event.target.files[0])}></input>
+            <input type='file' name='upload' id="upload" ref={uploadRef} onChange={(event) => setFile(event.target.files[0])}></input>
             <input type='radio' name='privacyLevel' value='public' id="public" checked={level === 'public'} onChange={(event) => onRadiosChange(event)}/><label htmlFor='public'>Public</label>
             <input type='radio' name='privacyLevel' value='protected' id="protected" checked={level === 'protected'} onChange={(event) => onRadiosChange(event)}/><label htmlFor='protected'>Protected</label>
             <input type='radio' name='privacyLevel' value='private' id="private" checked={level === 'private'} onChange={(event) => onRadiosChange(event)}/><label htmlFor='private'>Private</label>
